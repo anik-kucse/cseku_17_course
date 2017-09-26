d0aa60e13d414ecf718274d04af967dbf4c7c7ac
J. M. Ashfiqur Rahman <anikpro.exe@gmail.com>
2017-09-26 09:24:27 +0600

    starting account manage

--- app/views/accountmanage.php
+++ app/views/accountmanage.php
@@ -3,163 +3,189 @@
     <center><h1>Manage Your Account</h1></center>
   </header>
   <br>
-  <form>
-    <div class="form-group row">
+  <div class="form-group row">
       <label for="inputPassword" class="col-sm-4 col-form-label">User Name</label>
       <div class="col-sm-7">
-        <label class="form-control" id="inputPassword" >rCr_BD</label>
+          <label class="form-control" id="inputPassword" >rCr_BD</label>
       </div>
-    </div>
-    <div class="form-group row">
+  </div>
+  <div class="form-group row">
       <label for="inputPassword" class="col-sm-4 col-form-label">First Name</label>
       <div class="col-sm-7">
-        <label class="form-control" id="inputPassword" >J.M.</label>
+          <label class="form-control" id="inputPassword" >J.M.</label>
       </div>
-    </div>
-    <div class="form-group row">
+  </div>
+  <div class="form-group row">
       <label for="inputPassword" class="col-sm-4 col-form-label">Middle Name</label>
       <div class="col-sm-7">
-        <label class="form-control" id="inputPassword" >Ashfiqur</label>
+          <label class="form-control" id="inputPassword" >Ashfiqur</label>
       </div>
-    </div>
-    <div class="form-group row">
+  </div>
+  <div class="form-group row">
       <label for="inputPassword" class="col-sm-4 col-form-label">Last Name</label>
       <div class="col-sm-7">
-        <label class="form-control" id="inputPassword" >Rahman</label>
+          <label class="form-control" id="inputPassword" >Rahman</label>
       </div>
-    </div>
-    <div class="form-group row">
+  </div>
+  <div class="form-group row">
       <label for="inputPassword" class="col-sm-4 col-form-label">Student ID</label>
       <div class="col-sm-7">
-        <label class="form-control" id="inputPassword" >150231</label>
+          <label class="form-control" id="inputPassword" >150231</label>
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputPassword" class="col-sm-4 col-form-label">Year</label>
-      <div class="col-sm-7">
-        <label class="form-control" id="inputPassword" >3rd year</label>
+  </div>
+  <div class="form-group row">
+      <label for="inputPassword" class="col-sm-4 col-form-label">Year - term</label>
+      <div class="col-sm-6">
+          <label class="form-control" id="inputPassword" >3rd year</label>
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputPassword" class="col-sm-4 col-form-label">Term</label>
-      <div class="col-sm-7">
-        <label class="form-control" id="inputPassword" >II</label>
+      <div class="col-sm-1">
+          <label class="form-text" ><a class="button" data-toggle="modal" href="#changeYearTerm">Edit!!</a></label>
+          <!-- Modal -->
+          <div class="modal fade" id="changeYearTerm" tabindex="-1" role="dialog" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title">Change Year Term</h5>
+                          <button type="button" class="btn btn-outline-customs" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                          </button>
+                      </div>
+                      <div class="modal-body">
+                          <form>
+                              <select class="form-control" id="singleselect" name="ddlYearTerm">
+                                  <?php
+                                  foreach ($data[0] as $key => $value){
+                                      ?>
+                                      <option value="<?php echo $value['id']?>"><?php echo $value['year']." year ".$value['term']." term" ?></option>
+                                      <?php
+                                  }
+                                  ?>
+                              </select>
+                              <br><br>
+                              <div class="input-group row col-12 col-md-12">
+                                  <label class="col-5 col-md-5"></label>
+                                  <button type="submit" class="btn btn-outline-customs">Save Changed</button>
+                              </div>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
-    </div>
-    <div class="form-group row">
+  </div>
+  <div class="form-group row">
       <label for="inputPassword" class="col-sm-4 col-form-label">Email</label>
       <div class="col-sm-6">
-        <label class="form-control" id="inputPassword" >anikpro.exe@gmail.com</label>
+          <label class="form-control" id="inputPassword" >anikpro.exe@gmail.com</label>
       </div>
       <div class="col-sm-1">
-        <label class="form-text" ><a class="button" data-toggle="modal" href="#changeEmail">Edit!!</a></label>
-        <!-- Modal -->
-        <div class="modal fade" id="changeEmail" tabindex="-1" role="dialog" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Change Email Address</h5>
-                <button type="button" class="btn btn-outline-customs" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
+          <label class="form-text" ><a class="button" data-toggle="modal" href="#changeEmail">Edit!!</a></label>
+          <!-- Modal -->
+          <div class="modal fade" id="changeEmail" tabindex="-1" role="dialog" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title">Change Email Address</h5>
+                          <button type="button" class="btn btn-outline-customs" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                          </button>
+                      </div>
+                      <div class="modal-body">
+                          <form>
+                              <div class="input-group row col-12 col-md-12">
+                                  <input type="text" class="form-control" placeholder="Current Email"  required="1" aria-describedby="basic-addon2">
+                              </div>
+                              <br><br>
+                              <div class="input-group row col-12 col-md-12">
+                                  <input type="text" class="form-control" placeholder="New Email"   required="1" aria-describedby="basic-addon2">
+                              </div>
+                              <br><br>
+                              <div class="input-group row col-12 col-md-12">
+                                  <label class="col-5 col-md-5"></label>
+                                  <button type="submit" class="btn btn-outline-customs">Save Changed</button>
+                              </div>
+                          </form>
+                      </div>
+                  </div>
               </div>
-              <div class="modal-body">
-                <form>
-                  <div class="input-group row col-12 col-md-12">
-                    <input type="text" class="form-control" placeholder="Current Email"  required="1" aria-describedby="basic-addon2">
-                  </div>
-                  <br><br>
-                  <div class="input-group row col-12 col-md-12">
-                    <input type="text" class="form-control" placeholder="New Email"   required="1" aria-describedby="basic-addon2">
-                  </div>
-                  <br><br>
-                  <div class="input-group row col-12 col-md-12">
-                    <label class="col-5 col-md-5"></label>
-                    <button type="submit" class="btn btn-outline-customs">Save Changed</button>
-                  </div>
-                </form>
-              </div>
-            </div>
           </div>
-        </div>
       </div>
-    </div>
-    <div class="form-group row">
+  </div>
+  <div class="form-group row">
       <label for="inputPassword" class="col-sm-4 col-form-label">Mobile</label>
       <div class="col-sm-6">
-        <label class="form-control" id="inputPassword" >01314307248</label>
+          <label class="form-control" id="inputPassword" >01314307248</label>
       </div>
       <div class="col-sm-1">
-        <label class="form-text" ><a class="button" data-toggle="modal" href="#changeMobile">Edit!!</a></label>
-        <!-- Modal -->
-        <div class="modal fade" id="changeMobile" tabindex="-1" role="dialog" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Change Mobile Number</h5>
-                <button type="button" class="btn btn-outline-customs" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
+          <label class="form-text" ><a class="button" data-toggle="modal" href="#changeMobile">Edit!!</a></label>
+          <!-- Modal -->
+          <div class="modal fade" id="changeMobile" tabindex="-1" role="dialog" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title">Change Mobile Number</h5>
+                          <button type="button" class="btn btn-outline-customs" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                          </button>
+                      </div>
+                      <div class="modal-body">
+                          <form>
+                              <div class="input-group row col-12 col-md-12">
+                                  <input type="text" class="form-control" placeholder="Current Mobile"   required="1" aria-describedby="basic-addon2">
+                              </div>
+                              <br><br>
+                              <div class="input-group row col-12 col-md-12">
+                                  <input type="text" class="form-control" placeholder="New Mobile"  required="1" aria-describedby="basic-addon2">
+                              </div>
+                              <br><br>
+                              <div class="input-group row col-12 col-md-12">
+                                  <label class="col-5 col-md-5"></label>
+                                  <button type="submit" class="btn btn-outline-customs">Save Changed</button>
+                              </div>
+                          </form>
+                      </div>
+                  </div>
               </div>
-              <div class="modal-body">
-                <form>
-                  <div class="input-group row col-12 col-md-12">
-                    <input type="text" class="form-control" placeholder="Current Mobile"   required="1" aria-describedby="basic-addon2">
-                  </div>
-                  <br><br>
-                  <div class="input-group row col-12 col-md-12">
-                    <input type="text" class="form-control" placeholder="New Mobile"  required="1" aria-describedby="basic-addon2">
-                  </div>
-                  <br><br>
-                  <div class="input-group row col-12 col-md-12">
-                    <label class="col-5 col-md-5"></label>
-                    <button type="submit" class="btn btn-outline-customs">Save Changed</button>
-                  </div>
-                </form>
-              </div>
-            </div>
           </div>
-        </div>
       </div>
-    </div>
-    <div class="form-group row">
+  </div>
+  <div class="form-group row">
       <label for="inputPassword" class="col-sm-4 col-form-label"></label>
       <div class="col-sm-6">
-        <p>Do you wanna change your password? <a class="button" data-toggle="modal" href="#changePassword">Yes!!</a></p>
-        <!-- Modal -->
-        <div class="modal fade" id="changePassword" tabindex="-1" role="dialog" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Change Your Password</h5>
-                <button type="button" class="btn btn-outline-customs" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
+          <p>Do you wanna change your password? <a class="button" data-toggle="modal" href="#changePassword">Yes!!</a></p>
+          <!-- Modal -->
+          <div class="modal fade" id="changePassword" tabindex="-1" role="dialog" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title">Change Your Password</h5>
+                          <button type="button" class="btn btn-outline-customs" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                          </button>
+                      </div>
+                      <div class="modal-body">
+                          <form>
+                              <div class="Form-group row col-12 col-md-12">
+                                  <input type="password" class="form-control" placeholder="Current Password"  required="1" aria-describedby="basic-addon2">
+                              </div>
+                              <br>
+                              <div class="Form-group row col-12 col-md-12">
+                                  <input type="password" class="form-control" placeholder="New Password"  required="1" aria-describedby="basic-addon2">
+                              </div>
+                              <br>
+                              <div class="Form-group row col-12 col-md-12">
+                                  <input type="password" class="form-control" placeholder="Repeat New Password"  required="1" aria-describedby="basic-addon2">
+                              </div>
+                              <br>
+                              <div class="input-group row col-12 col-md-12">
+                                  <label class="col-5 col-md-5"></label>
+                                  <button type="submit" class="btn btn-outline-customs">Save Changed</button>
+                              </div>
+                          </form>
+                      </div>
+                  </div>
               </div>
-              <div class="modal-body">
-                <form>
-                  <div class="Form-group row col-12 col-md-12">
-                    <input type="password" class="form-control" placeholder="Current Password"  required="1" aria-describedby="basic-addon2">
-                  </div>
-                  <br>
-                  <div class="Form-group row col-12 col-md-12">
-                    <input type="password" class="form-control" placeholder="New Password"  required="1" aria-describedby="basic-addon2">
-                  </div>
-                  <br>
-                  <div class="Form-group row col-12 col-md-12">
-                    <input type="password" class="form-control" placeholder="Repeat New Password"  required="1" aria-describedby="basic-addon2">
-                  </div>
-                  <br>
-                  <div class="input-group row col-12 col-md-12">
-                    <label class="col-5 col-md-5"></label>
-                    <button type="submit" class="btn btn-outline-customs">Save Changed</button>
-                  </div>
-                </form>
-              </div>
-            </div>
           </div>
-        </div>
       </div>
-    </div>
-  </form>
+  </div>
 </div> <!--content-->
