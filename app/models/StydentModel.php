3f09381325525077d36a5de50d9d347ef0ab3bd4
J. M. Ashfiqur Rahman <anikpro.exe@gmail.com>
2017-09-26 09:33:01 +0600

    create base calsses

---
+++ app/models/StydentModel.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: anik
+ * Date: 9/26/2017
+ * Time: 9:31 AM
+ */
+class StydentModel extends MainModel{
+    public function __construct(){
+        parent::__construct();
+    }
+}
