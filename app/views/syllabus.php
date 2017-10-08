c2740cbc6096ad88dc5901240f7a7e283459c0c2
J. M. Ashfiqur Rahman <anikpro.exe@gmail.com>
2017-10-08 19:49:05 +0600

    fix

--- app/views/syllabus.php
+++ app/views/syllabus.php
@@ -2,55 +2,44 @@
     <br>
     <form method="POST" action="<?php echo BASE_URL?>/SyllabusPublic/search">
         <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Year - Term</label>
-            <div class="col-sm-10">
-                <select class="form-control" id="singleselect" name="ddlYearTerm">
-                    <?php
-                    foreach ($data[0] as $key => $value){
-                        ?>
-                        <option value="<?php echo $value['id']?>"
+            <div class="col-sm-6">
+                <label class="col-form-label">Year - Term</label>
+                <div class="">
+                    <select class="form-control" id="singleselect" name="ddlYearTerm">
                         <?php
-                        if (array_key_exists('term', $data)) {
-                            if($value['id'] == $data['term']){
-                                echo 'selected = "selected"';
-                            }
-                        }
-                        ?>>
-                            <?php echo $value['year']." year ".$value['term']." term" ?>
-                        </option>
-                        <?php
-                    }
-                    ?>
-                </select>
-            </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Syllabus</label>
-            <div class="col-sm-10">
-                <select class="form-control" id="singleselect" name="ddlSyllabusName">
-                    <?php
-                    foreach ($data[1] as $key => $value){
-                        ?>
-                        <option value="<?php echo $value['id']?>"
-                            <?php
-                            if (array_key_exists('syllabusName', $data)) {
-                                if($value['id'] == $data['syllabusName']){
+                        foreach ($data[0] as $key => $value){
+                            ?>
+                            <option value="<?php echo $value['id']?>" <?php
+                            if (array_key_exists('term', $data)) {
+                                if($value['id'] == $data['term']){
                                     echo 'selected = "selected"';
                                 }
                             }
-                            ?>
-                        >
-                            <?php echo $value['syllabus_Name'] ?>
-                        </option>
-                        <?php
-                    }
-                    ?>
-                </select>
+                            ?>>
+                                <?php echo $value['year']." year ".$value['term']." term" ?>
+                            </option>
+                            <?php
+                        }
+                        ?>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label class="col-sm-2 col-form-label"></label>
-            <div class="col-sm-10">
+            <div class="col-sm-5">
+                <label class="col-form-label">Syllabus</label>
+                <div class="">
+                    <select class="form-control" id="singleselect" name="ddlSyllabusName">
+                        <?php
+                        foreach ($data[1] as $key => $value){
+                            ?>
+                            <option value="<?php echo $value['id']?>"><?php echo $value['syllabus_Name'] ?></option>
+                            <?php
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-1">
+                <label class="col-form-label" style="visibility:hidden">Invisible</label>
                 <button type="submit" name="search" class="btn btn-outline-customs ">Search</button>
             </div>
         </div>
@@ -60,7 +49,7 @@
     for($i = 2; $i < 5; $i++){
         if(!empty($data[$i])){
             ?>
-            <span><b><?php echo $data[$i][0]['group_rule']?></b></span>
+            <center><h2><b><?php echo $data[$i][0]['group_rule']?></b></h2></center>
             <table class="table table-striped ">
                 <thead>
                 <tr>
