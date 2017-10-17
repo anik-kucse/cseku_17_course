0e7d3956850ae34336dd9b37d78ac2165854a96f
Anisha Tasnim <tasnimanisha@gmail.com>
2017-10-18 01:07:39 +0600

    requestedlist updated

--- app/views/admin/requestedlist.php
+++ app/views/admin/requestedlist.php
@@ -3,32 +3,6 @@
         <center><h1>Request Students</h1></center>
     </header>
     <br>
-    <form method="post" action="<?php echo BASE_URL?>/MyCourses/myCourses">
-        <div class="form-row">
-            <div class="col-md-2">
-                <label class="form-control-label">Year-Term</label>
-            </div>
-            <div class="form-group col-md-9">
-                <select class="form-control" name="ddlYearTerm">
-                    <?php
-                    foreach ($data[0] as $key => $value){
-                        ?>
-                        <!-- <?php if($value['id'] == $data['term']){echo 'selected = "selected"';}?> -->
-                        <option value="<?php echo $value['id']?>" <?php if($value['id'] == $data['term']){echo 'selected = "selected"';}?>>
-                            <?php echo $value['year']." year ".$value['term']." term"?>
-                        </option>
-                        <?php
-                    }
-                    ?>
-                </select>
-            </div>
-            <div class="form-group col-md-1" >
-                <div class="">
-                    <button type="submit" class="btn btn-outline-customs ">Select</button>
-                </div>
-            </div>
-        </div>
-    </form>
     <br>
     <form method="post">
         <div class = "form-group row ">
@@ -39,7 +13,10 @@
                     <tr>
                         <th>Student ID</th>
                         <th>Student Name</th>
+                        <th>Year</th>
+                        <th>Term</th>
                         <th>Status</th>
+                        <th>Session</th>
                         <th>Details</th>
                     </tr>
                     </thead>
@@ -47,22 +24,23 @@
                         <tr>
                             <td>150213</td>
                             <td>Anisha</td>
+                            <td>3rd</td>
+                            <td>II</td>
+                            <td>2016-17</td>
                             <td>Not Approved</td>
                             <td><a href="">Details</a></td>
                         </tr>
                         <tr>
                             <td>150213</td>
                             <td>Anisha</td>
-                            <td>Approved</td>
+                            <td>3rd</td>
+                            <td>II</td>
+                            <td>2016-17</td>
+                            <td>Not Approved</td>
                             <td><a href="">Details</a></td>
                         </tr>
                     </tbody>
                 </table>
-                <div class="form-group" >
-                    <center>
-                        <button type="submit" class="btn btn-outline-customs">Approved</button>
-                    </center>
-                </div>
             </div>
         </div>
     </form>
